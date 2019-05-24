027013053f1a1717eea0fbb4db26823c81e5cb3c
bristoll <admync@gmail.com>
2019-05-24 23:19:06 +0200

    *

--- ProyectoTablon/src/com/ted/model/UserDAOImpl.java
+++ ProyectoTablon/src/com/ted/model/UserDAOImpl.java
@@ -6,9 +6,12 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-
 import com.ted.connection.Conection;
-
+/**
+ * 
+ * @author Cristo
+ *
+ */
 
 
 public class UserDAOImpl implements UserDAO {
