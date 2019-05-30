ff6b1bcfe350133291ba06fb0546267388a04275
bristoll <admync@gmail.com>
2019-05-30 16:57:38 +0200

    añadida estructura servlets y package utils

---
+++ ProyectoTablon/src/com/ted/utils/ComprobarPerfil.java
@@ -0,0 +1,11 @@
+package com.ted.utils;
+
+public class ComprobarPerfil implements Comprobar {
+
+	@Override
+	public boolean comprobar() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
