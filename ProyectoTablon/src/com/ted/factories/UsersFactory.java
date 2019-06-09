57216b8a5b533b2a0fec059138034ff46bdfa3f4
bristoll <admync@gmail.com>
2019-06-09 13:54:06 +0200

    main changes

--- ProyectoTablon/src/com/ted/factories/UsersFactory.java
+++
@@ -1,5 +0,0 @@
-package com.ted.factories;
-
-public class UsersFactory {
-
-}
--- ProyectoTablon/src/com/ted/utils/Comprobar.java
+++
@@ -1,6 +0,0 @@
-package com.ted.utils;
-@FunctionalInterface
-public interface Comprobar {
-	public  abstract boolean comprobar();
-
-}
